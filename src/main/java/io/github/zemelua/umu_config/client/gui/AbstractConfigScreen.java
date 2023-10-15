cc5af20154173bf5602b67ae41e3354df65be08f
Zemelua <zemelua1107@gmail.com>
2023-10-15 15:37:30 +0900

    ✨MCバージョンを1.20.2に更新！

--- src/main/java/io/github/zemelua/umu_config/client/gui/AbstractConfigScreen.java
+++ src/main/java/io/github/zemelua/umu_config/client/gui/AbstractConfigScreen.java
@@ -53,7 +53,7 @@ public abstract class AbstractConfigScreen extends Screen {
 
 	@Override
 	public void render(DrawContext context, int mouseX, int mouseY, float delta) {
-		this.renderBackground(context);
+		this.renderBackground(context, mouseX, mouseY, delta);
 
 		super.render(context, mouseX, mouseY, delta);
 
--- src/main/java/io/github/zemelua/umu_config/client/gui/ConfigsScreen.java
+++ src/main/java/io/github/zemelua/umu_config/client/gui/ConfigsScreen.java
@@ -61,7 +61,7 @@ public class ConfigsScreen extends Screen {
 
 	@Override
 	public void render(DrawContext context, int mouseX, int mouseY, float delta) {
-		this.renderBackground(context);
+		this.renderBackground(context, mouseX, mouseY, delta);
 
 		super.render(context, mouseX, mouseY, delta);
 
