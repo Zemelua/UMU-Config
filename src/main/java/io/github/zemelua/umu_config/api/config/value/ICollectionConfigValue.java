56fc5c8646d887c39e33a9e9ff37e0e201bf758d
Zemelua <red0165333@gmail.com>
2023-12-31 22:49:45 +0900

    Collection Config Valueを実装

---
+++ src/main/java/io/github/zemelua/umu_config/api/config/value/ICollectionConfigValue.java
@@ -0,0 +1,6 @@
+package io.github.zemelua.umu_config.api.config.value;
+
+import java.util.Collection;
+
+public interface ICollectionConfigValue<E, T extends Collection<E>> extends IConfigValue<T> {
+}
